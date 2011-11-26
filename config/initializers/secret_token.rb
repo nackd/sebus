3b6b32e5a275e0baf15f97883d36ae07c84358e0
Jose M. Prieto <jmprieto@gmx.net>
2011-11-26 12:59:00 +0100

    rails new tussam --skip-active-record

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tussam::Application.config.secret_token = '904a0b20e053bd7d24ee01ac91f75c9c441e837a9a51d1fac3102e4bfc100407738b68b2fa25f90447f3258cd956b3e75789c82c5faa896156279c672a250db6'
