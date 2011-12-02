ef7b463855afe30f640d47d16688b3c94f52b99a
Jose M. Prieto <jmprieto@gmx.net>
2011-12-02 21:20:33 +0100

    estimate!

--- app/controllers/tussam_controller.rb
+++ app/controllers/tussam_controller.rb
@@ -4,23 +4,9 @@ class TussamController < ApplicationController
   end
 
   def estimate
-    Savon.env_namespace = :soap
-    client = Savon::Client.new do
-      wsdl.endpoint = 'http://www.infobustussam.com:9001/services/dinamica.asmx'
-      wsdl.namespace = 'http://tempuri.org/'
-      http.open_timeout = 3
-      http.read_timeout = 5
-    end
-
-    response = client.request 'wsdl:GetPasoParada' do
-      http.headers["SOAPAction"] = '"http://tempuri.org/GetPasoParada"'
-      soap.body = {:"wsdl:linea" => params[:estimate][:line],
-                   :"wsdl:parada" => params[:estimate][:stop],
-                   :"wsdl:status" => 1}
-    end
-
-    estimation = response[:get_paso_parada_response][:get_paso_parada_result][:paso_parada]
-    @e1 = estimation[:e1][:minutos]
-    @e2 = estimation[:e2][:minutos]
+    @stop = Stop.where(number: params[:id].to_i).first
+    @estimate = @stop.lines.inject({}){ |hash, line|
+      hash.merge({line => Tussam.estimate(@stop.number, line)})
+    }
   end
 end
--- lib/tussam.rb
+++ lib/tussam.rb
@@ -25,4 +25,27 @@ module Tussam
   def self.update_stops_if_empty
     update_stops if Stop.count == 0
   end
+
+  def self.estimate(stop, line)
+    Savon.env_namespace = :soap
+    client = Savon::Client.new do
+      wsdl.endpoint = 'http://www.infobustussam.com:9001/services/dinamica.asmx'
+      wsdl.namespace = 'http://tempuri.org/'
+      http.open_timeout = 3
+      http.read_timeout = 5
+    end
+
+    response = client.request 'wsdl:GetPasoParada' do
+      http.headers["SOAPAction"] = '"http://tempuri.org/GetPasoParada"'
+      soap.body = {:"wsdl:linea"  => line,
+                   :"wsdl:parada" => stop,
+                   :"wsdl:status" => 1}
+    end
+    estimation = response[:get_paso_parada_response][:get_paso_parada_result][:paso_parada]
+
+    [{:time => estimation[:e1][:minutos], :distance => estimation[:e1][:metros]},
+     {:time => estimation[:e2][:minutos], :distance => estimation[:e2][:metros]}]
+  rescue NoMethodError, Timeout::Error
+    [{:time => nil, :distance => nil}, {:time => nil, :distance => nil}]
+  end
 end
