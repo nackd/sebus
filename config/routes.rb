ed31e9364f896ce81c7b63164e62198f8a77070f
Jose M. Prieto <jmprieto@gmx.net>
2011-12-08 20:03:02 +0100

    add the about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Tussam::Application.routes.draw do
+  root :to            => "tussam#search"
   get  "search"       => "tussam#search"
   post "estimate"     => "tussam#estimate", :as => :post_estimate
   get  "location"     => "tussam#location"
   post "stops"        => "tussam#stops"
   get  "estimate/:id" => "tussam#estimate", :as => :tussam_estimate
-  root :to            => "tussam#search"
+  get  "about"        => "tussam#about"
 end
