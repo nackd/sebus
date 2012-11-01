1c68fbdc788ba02b62959649e18b8a4e4638fb8f
Jose M. Prieto <jmprieto@gmx.net>
2012-11-02 00:07:44 +0100

    change some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Tussam::Application.routes.draw do
-  root :to            => "tussam#search"
-  get  "search"       => "tussam#search"
+  root :to            => "tussam#type"
   post "estimate"     => "tussam#estimate", :as => :post_estimate
+  get  "estimate/:id" => "tussam#estimate", :as => :tussam_estimate
   get  "location"     => "tussam#location"
   post "stops"        => "tussam#stops"
-  get  "estimate/:id" => "tussam#estimate", :as => :tussam_estimate
   get  "about"        => "tussam#about"
 end
